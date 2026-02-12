83cfe95fd0e10fd4bf7e268669f5d1c9d2ea2f59
Alyssa Verkade <alyssaverkade@gmail.com>
2026-02-12 13:30:50 -0800

    configure gruvbox better, add colorizer

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -2,10 +2,32 @@ return {
   { "junegunn/fzf", dir = "~/.fzf", build = "./install --all" },
   "junegunn/fzf.vim" ,
   {
-    "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = {}
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000 ,
+    config = true,
+    opts = {
+      overrides = {},
+      terminal_colors = true,
+      contrast = "hard",
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = false,
+        operators = false,
+        folds = false,
+      }
+    }
   },
   "luochen1990/rainbow",
   "sheerun/vim-polyglot",
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require('colorizer').setup()
+    end,
+    priority = 999,
+    opts = {},
+  },
   { "tamton-aquib/staline.nvim", opts = {} },
   "editorconfig/editorconfig-vim",
   "wellle/targets.vim",
